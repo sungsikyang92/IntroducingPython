3de4981d2d62dcf43851104798144f059264c186
sungsikyang92 <sungsik.yang92@gmail.com>
2021-04-03 02:48:09 +0900

    Introducing Python chap17 done

---
+++ zmq_server.py
@@ -0,0 +1,14 @@
+import zmq
+
+host = '127.0.0.1'
+port = 6789
+context = zmq.Context()
+server = context.socket(zmq.Rserver.bind("tcp://%s:%s" % (host, port)))
+while True:
+    # 클라이언트에서 다음 요청을 기다린다.
+    request_bytes = server.recv()
+    request_str = request_bytes.decode('utf-8')
+    print("That voice in my head says: %s" % request_str)
+    reply_str = "Stop saying: %s" % request_str
+    reply_bytes = bytes(reply_str, 'utf-8')
+    server.send(reply_bytes)
